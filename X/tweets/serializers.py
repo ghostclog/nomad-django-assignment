c02cc95b013d99f86bed3a6c6bf05e013a5615f0
ghostclog <kdy11281@gmail.com>
2024-10-04 01:05:02 +0900

    간단한 변경사항들

--- X/tweets/serializers.py
+++ X/tweets/serializers.py
@@ -1,9 +1,4 @@
 from rest_framework import serializers
-from users.models import User
-
-class UserSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    user_nickname = serializers.CharField(max_length=50)
 
 class TweetSerializer(serializers.Serializer):
     pk = serializers.IntegerField(read_only=True)
